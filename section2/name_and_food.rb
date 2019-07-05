73732d36ec6a29e01d3717436310bb70bc225397
Eric Spence <ericspence@gmail.com>
2019-07-05 16:49:14 -0700

    Update name_and_food.rb

--- section2/name_and_food.rb
+++ section2/name_and_food.rb
@@ -1,3 +1,8 @@
+# <summary>
+# Udemy course - Complete Ruby Programmer
+# Project 1 - Print name and favorite food
+# </summary>
+
 puts "My name is: Eric\n"
 
 puts "My favorite food is: Cheeseburgers"
